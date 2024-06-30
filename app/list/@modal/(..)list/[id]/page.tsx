efa9c5ccd1415f695f442ab29e3099863edb9d44
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-06-30 21:10:04 +0600

    chore: remove unnecessary generate metadata function

--- app/list/@modal/(..)list/[id]/page.tsx
+++ app/list/@modal/(..)list/[id]/page.tsx
@@ -1,14 +1,6 @@
 import { Modal } from "~~/app/list/@modal/modal";
 import { Url } from "~~/model/url-model";
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const url = await Url.findOne({ _id: params.id });
-  return {
-    title: `Mini URL Shortener - List | analytics-${url.shortId}`,
-    description: `Mini URL Shortener - List | analytics-${url.redirectUrl}`,
-  };
-}
-
 const Analytics = async ({
   params,
 }: {
