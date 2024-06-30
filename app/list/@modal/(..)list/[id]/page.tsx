644b0609cbba942270db5e174de79845a6f05b5c
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-06-30 20:50:33 +0600

    chore: added recharts

--- app/list/@modal/(..)list/[id]/page.tsx
+++ app/list/@modal/(..)list/[id]/page.tsx
@@ -1,10 +1,14 @@
 import { Modal } from "~~/app/list/@modal/modal";
+import { Url } from "~~/model/url-model";
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
+  const url = await Url.findOne({ _id: params.id });
   return {
-    title: "Mini URL Shortener | List | details",
+    title: `Mini URL Shortener - List | details-${url.shortId}`,
+    description: `Mini URL Shortener - List | details-${url.redirectUrl}`,
   };
 }
+
 const page = () => {
   return (
     <Modal title="details">
