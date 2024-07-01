9210b85a80b536be234f44851dbceddc0884a813
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-07-01 22:08:32 +0600

    fix: chart placement in mobile

--- app/list/@modal/(..)list/[id]/page.tsx
+++ app/list/@modal/(..)list/[id]/page.tsx
@@ -41,85 +41,91 @@ const Analytics = async ({
 
   return (
     <Modal title={`Analytics of ${url.redirectUrl}`}>
-      <ScrollArea className="flex flex-col space-y-4 h-[90vh]">
-        <p className="flex items-center gap-2">
-          {" "}
-          <span className="text-nowrap"> Original Url : </span>
-          <Link
-            title={url.redirectUrl}
-            href={url.redirectUrl}
-            className="flex items-center hover:text-primary truncate"
-          >
-            {url.redirectUrl}{" "}
-            <ArrowTopRightIcon className="cursor-pointer size-4 " />
-          </Link>{" "}
-        </p>
-        <p className="flex items-center gap-2">
-          {" "}
-          Generated Short id :{" "}
-          <Link
-            title={url.shortId}
-            href={`${hostUrl}/${url.shortId}`}
-            className="flex items-center hover:text-primary"
-          >
-            {url.shortId}{" "}
-            <ArrowTopRightIcon className="cursor-pointer size-4 " />
-          </Link>{" "}
-        </p>
-        <p className="flex items-center gap-2 ">
-          {" "}
-          <span className="text-nowrap"> generated Short Url : </span>
-          <Link
-            title={`${hostUrl}/${url.shortId}`}
-            href={`${hostUrl}/${url.shortId}`}
-            className="flex items-center hover:text-primary truncate"
-          >
-            {hostUrl}/{url.shortId}
-            <ArrowTopRightIcon className="cursor-pointer size-4 " />
-          </Link>{" "}
-        </p>
-        <div className="size-40 my-4">
-          <QrCode value={`${hostUrl}/${url.shortId}`} />
-        </div>
-        <p className="font-bold text-lg">Page view using device analytics</p>
-        <div className="mt-3 h-[30vh] w-full">
-          <Chart data={chartData} />
-        </div>
-        <p className="font-bold text-lg">Visited History</p>
-        {url.visitHistory.length === 0 ? (
-          <div className="flex items-center justify-center flex-col">
-            <Image
-              src="/not-found.svg"
-              width={100}
-              height={100}
-              alt="No visit history recorded yet."
-            />
-            <p className="text-gray-500">No visit history recorded yet.</p>
+      <ScrollArea className=" h-[90vh] ">
+        <div className="flex flex-col space-y-4">
+          <p className="flex items-center gap-2">
+            {" "}
+            <span className="text-nowrap"> Original Url : </span>
+            <Link
+              title={url.redirectUrl}
+              href={url.redirectUrl}
+              className="flex items-center hover:text-primary truncate"
+            >
+              {url.redirectUrl}{" "}
+              <ArrowTopRightIcon className="cursor-pointer size-4 " />
+            </Link>{" "}
+          </p>
+          <p className="flex items-center gap-2">
+            {" "}
+            Generated Short id :{" "}
+            <Link
+              title={url.shortId}
+              href={`${hostUrl}/${url.shortId}`}
+              className="flex items-center hover:text-primary"
+            >
+              {url.shortId}{" "}
+              <ArrowTopRightIcon className="cursor-pointer size-4 " />
+            </Link>{" "}
+          </p>
+          <p className="flex items-center gap-2 ">
+            {" "}
+            <span className="text-nowrap"> generated Short Url : </span>
+            <Link
+              title={`${hostUrl}/${url.shortId}`}
+              href={`${hostUrl}/${url.shortId}`}
+              className="flex items-center hover:text-primary truncate"
+            >
+              {hostUrl}/{url.shortId}
+              <ArrowTopRightIcon className="cursor-pointer size-4 " />
+            </Link>{" "}
+          </p>
+          <div className="size-40 my-4">
+            <QrCode value={`${hostUrl}/${url.shortId}`} />
           </div>
-        ) : (
-          <ScrollArea className="h-[40vh]">
-            <div className="flex flex-col space-y-4">
-              {url.visitHistory.map((visit: VisitType) => (
-                <div key={visit._id} className="flex flex-col space-y-2">
-                  <p className="flex items-center gap-2">
-                    {" "}
-                    <span className="text-nowrap"> Timestamp : </span>
-                    <span>
-                      {new Date(visit.timestamp).toLocaleString()}
-                    </span>{" "}
-                  </p>
-                  <p className="flex items-center gap-2">
-                    {" "}
-                    <span className="text-nowrap"> Visit using : </span>
-                    <span>
-                      {visit.deviceType ?? "Unknown device type"}
-                    </span>{" "}
-                  </p>
-                </div>
-              ))}
+          <p className="font-bold text-sm sm:text-lg text-left">
+            Page view using device analytics
+          </p>
+          <div className="mt-3 h-[25vh] w-3/4">
+            <Chart data={chartData} />
+          </div>
+          <p className="font-bold text-sm sm:text-lg text-left">
+            Visited History
+          </p>
+          {url.visitHistory.length === 0 ? (
+            <div className="flex items-center justify-center flex-col">
+              <Image
+                src="/not-found.svg"
+                width={100}
+                height={100}
+                alt="No visit history recorded yet."
+              />
+              <p className="text-gray-500">No visit history recorded yet.</p>
             </div>
-          </ScrollArea>
-        )}
+          ) : (
+            <ScrollArea className="h-[40vh]">
+              <div className="flex flex-col space-y-4">
+                {url.visitHistory.map((visit: VisitType) => (
+                  <div key={visit._id} className="flex flex-col space-y-2">
+                    <p className="flex items-center gap-2">
+                      {" "}
+                      <span className="text-nowrap"> Timestamp : </span>
+                      <span>
+                        {new Date(visit.timestamp).toLocaleString()}
+                      </span>{" "}
+                    </p>
+                    <p className="flex items-center gap-2">
+                      {" "}
+                      <span className="text-nowrap"> Visit using : </span>
+                      <span>
+                        {visit.deviceType ?? "Unknown device type"}
+                      </span>{" "}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </ScrollArea>
+          )}
+        </div>
       </ScrollArea>
     </Modal>
   );
